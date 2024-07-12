ce6133d7cd36aec8926caa83019005642cda47a1
xAzke <diego.riquelme2016@gmail.com>
2024-07-12 17:26:34 -0400

    Agregadas las vistas y las plantillas de cada una

--- Contenido/urls.py
+++ Contenido/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-	path('index', views.index, name="index"),
-]
+	path('', views.vistaInicio, name="index"),
+	path('libros', views.vistaLibros, name="libros"),
+	path('autores', views.vistaAutores, name="autores"),
+	path('categorias', views.vistaCategorias, name="categorias"),
+] 
--- Contenido/views.py
+++ Contenido/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+def vistaInicio(request):
+    context = {
+        "Navbar": NavItem.objects.all()
+    }
+    return render(request, 'content/inicio.html', context)
+
+def vistaLibros(request):
+    context = {}
+    return render(request, 'content/libros.html')
+
+def vistaAutores(request):
+    context = {}
+    return render(request, 'content/autores.html')
+
+def vistaCategorias(request):
+    context = {}
+    return render(request, 'content/categorias.html')
